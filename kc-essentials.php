83cd89825c0fa9b52507c11f3dddfa44df7d4de5
Dzikri Aziz <kucrut@kucrut.org>
2011-10-22 22:45:07 +0700

    Initial

---
+++ kc-essentials.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Plugin name: KC Essentials
+Plugin URI: http://kucrut.org/
+Description: The essentials
+Version: 0.1
+Author: Dzikri Aziz
+Author URI: http://kucrut.org/
+License: GPL v2
+
+*/
+
+
+class kcEssentials {
+}
+
+?>
