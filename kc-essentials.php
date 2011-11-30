94d182fa5accf398c11a27d8c1dfa64d14c9827b
Dzikri Aziz <kucrut@kucrut.org>
2011-11-30 23:15:08 +0700

    Require KC Settings

--- kc-essentials.php
+++ kc-essentials.php
@@ -43,28 +43,17 @@ class kcEssentials {
 	}
 
 
-	private static function _options() {
-		$options = array(
-			'general'		=> array(
-				'components'	=> array( 'custom_widget_id_class', 'widgets', 'insert_custom_size' ),
-			),
-			'widgets'		=> array(
-				'widgets'		> array( 'post' )
-			)
-		);
-
-		return apply_filters( 'kc_essentials_setting', $options );
-	}
-
-
 	static function init() {
-		$settings = ( class_exists('kcSettings') ) ? kc_get_option( 'kc_essentials' ) : self::_options();
-		if ( empty($settings) || !isset($settings['general']) || empty($settings['general']) )
+		$paths = kcSettings::_paths( __FILE__ );
+		if ( !is_array($paths) )
 			return false;
 
+		self::$data['paths'] = $paths;
+
+		$settings = kc_get_option( 'kc_essentials' );
 		self::$data['settings'] = $settings;
-		self::$data['paths'] = self::_paths();
-		require_once self::$data['paths']['inc'] . '/_helpers.php';
+
+		require_once "${paths['inc']}/_helpers.php";
 
 		# Components
 		if ( isset($settings['general']['components']) && !empty($settings['general']['components']) ) {
@@ -72,7 +61,7 @@ class kcEssentials {
 				if ( $c == 'widgets' )
 					continue;
 
-				require_once self::$data['paths']['inc'] . "/{$c}.php";
+				require_once "${paths['inc']}/{$c}.php";
 				add_action( 'init', array("kcEssentials_{$c}", 'init'), 99 );
 			}
 		}
@@ -80,7 +69,7 @@ class kcEssentials {
 		# Helpers
 		if ( isset($settings['general']['helper']) && !empty($settings['general']['helper']) ) {
 			foreach ( $settings['general']['helper'] as $h )
-				require_once self::$data['paths']['inc'] . "/helper_{$h}.php";
+				require_once "${paths['inc']}/helper_{$h}.php";
 		}
 
 		# Dev
@@ -93,6 +82,28 @@ class kcEssentials {
 	}
 
 
+	# Register to KC Settings
+	public static function _activate() {
+		if ( !class_exists('kcSettings') )
+			wp_die( 'Please install and activate <a href="http://wordpress.org/extend/plugins/kc-settings/">KC Settings</a> before activating this plugin.<br /> <a href="'.wp_get_referer().'">&laquo; Go back</a> to plugins page.' );
+
+		$kcs = get_option('kc_settings');
+		$kcs['kids']['kc-essentials'] = array(
+			'name'	=> 'KC Essentials',
+			'file'	=> kc_plugin_file( __FILE__ )
+		);
+		update_option( 'kc_settings', $kcs );
+	}
+
+
+	# Unregister from KC Settings
+	public static function _deactivate() {
+		$kcs = get_option('kc_settings');
+		unset( $kcs['kids']['kc-essentials'] );
+		update_option( 'kc_settings', $kcs );
+	}
+
+
 	static function dev() {
 		echo '<pre>';
 
@@ -102,9 +113,29 @@ class kcEssentials {
 	}
 }
 
+
 require_once dirname(__FILE__) . '/kc-essentials-inc/options.php';
 add_action( 'init', array('kcEssentials', 'init'), 12 );
 
+
+# A hack for symlinks
+if ( !function_exists('kc_plugin_file') ) {
+	function kc_plugin_file( $file ) {
+		if ( !file_exists($file) )
+			return $file;
+
+		$file_info = pathinfo( $file );
+		$parent = basename( $file_info['dirname'] );
+
+		$file = ( $parent == $file_info['filename'] ) ? "{$parent}/{$file_info['basename']}" : $file_info['basename'];
+
+		return $file;
+	}
+}
+
+$plugin_file = kc_plugin_file( __FILE__ );
+register_activation_hook( $plugin_file, array('kcEssentials', '_activate') );
+register_deactivation_hook( $plugin_file, array('kcEssentials', '_deactivate') );
 
 require_once dirname(__FILE__) . '/kc-essentials-inc/widgets.php';
 add_action( 'widgets_init', array('kcEssentials_widgets', 'init') );
