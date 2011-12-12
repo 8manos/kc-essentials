bffc416afbee1c3a3f4fc68890915680ec6c8810
Dzikri Aziz <kucrut@kucrut.org>
2011-12-12 19:24:28 +0700

    Fixed Insert image with custom sizes

--- kc-essentials-inc/insert_custom_size.php
+++ kc-essentials-inc/insert_custom_size.php
@@ -7,22 +7,21 @@
 
 
 function kc_essentials_insert_custom_image_sizes( $fields, $post ) {
-	$image_sizes = kc_essentials_get_additional_image_sizes();
-	if ( empty($image_sizes) || !isset($fields['image-size']['html']) || substr($post->post_mime_type, 0, 5) != 'image' )
+	$_sizes = kc_essentials_get_additional_image_sizes();
+	if ( empty($_sizes) || !isset($fields['image-size']['html']) || substr($post->post_mime_type, 0, 5) != 'image' )
 		return $fields;
 
 	$items = array();
-	foreach ( array_keys($image_sizes) as $size ) {
-		$downsize = image_downsize( $post->ID, $size );
-		$enabled = $downsize[3];
-		$css_id = "image-size-{$size}-{$post->ID}";
-		$label = apply_filters( 'kc_image_size_name', $size );
+	foreach ( array_keys($_sizes) as $size ) {
+		$img = image_get_intermediate_size( $post->ID, $size );
+		if ( !$img )
+			continue;
 
-		$html  = "<div class='image-size-item'>\n";
-		$html .= "\t<input type='radio' " . disabled( $enabled, false, false ) . "name='attachments[{$post->ID}][image-size]' id='{$css_id}' value='{$size}' />\n";
-		$html .= "\t<label for='{$css_id}'>{$label}</label>\n";
-		if ( $enabled )
-			$html .= "\t<label for='{$css_id}' class='help'>" . sprintf( "(%d&nbsp;&times;&nbsp;%d)", $downsize[1], $downsize[2] ). "</label>\n";
+		$css_id = "image-size-{$size}-{$post->ID}";
+		$html  = "<div class='image-size-item'>";
+		$html .= "<input type='radio' name='attachments[{$post->ID}][image-size]' id='{$css_id}' value='{$size}' />";
+		$html .= "<label for='{$css_id}'>{$size}</label>";
+		$html .= "<label for='{$css_id}' class='help'>" . sprintf( "(%d&nbsp;&times;&nbsp;%d)", $img['width'], $img['height'] ). "</label>";
 		$html .= "</div>";
 
 		$items[] = $html;
