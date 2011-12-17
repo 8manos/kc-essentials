bf39948b48acd27858913e281bb862ab65b31e2f
Dzikri Aziz <kucrut@kucrut.org>
2011-12-17 22:09:27 +0800

    Merged new help-popup.js

--- kc-essentials-inc/scripts/help-popup.dev.js
+++ kc-essentials-inc/scripts/help-popup.dev.js
@@ -1,49 +1,57 @@
-jQuery(document).ready(function($) {
-	var $body	= $('body'),
-			$main	= $('.contextual-help-tabs-wrap').children(),
-			$side	= $('.contextual-help-sidebar'),
-			help	= '',
-			$box	= null;
+var win = window.dialogArguments || opener || parent || top;
 
-	if ( $main.length ) {
-		$main.each(function() {
-			help += '<h3 class="title">'+ $.trim( $('#tab-link-'+this.id.replace('tab-panel-', '')).text() ) +'</h3>';
-			help += $(this).html();
-		});
-	}
-	// Side help
-	if ( $side.children().length ) {
-		help += '<hr />'+ $side.html();
-	}
+(function($) {
+	win.kcBody = $(document.body);
 
-	if ( help !== '' ) {
-		$box = $('<div id="kc-help-popup" class="hidden"><div class="_wrap"><div class="_inside">'+help+'</div></div></div>').appendTo('body');
-
-		$(document).bind('keypress', function(e) {
-			if ( !$(e.target).is(':input') && e.which == 63 ) {
-				if ( $box.is(':visible') ) {
-					$box.trigger('close');
-				}
-				else {
-					$box.css({
-						width: $body.width() * .85,
-						height: $body.height() * .85
-					})
-					.lightbox_me({
-						centered: true,
-						destroyOnClose: true,
-						showOverlay: true,
-						overlaySpeed: 10,
-						lightboxSpeed: 10,
-						overlayCSS: {
-							background: '#fff',
-							opacity: '.1'
-						}
-					});
-				}
-
-				e.preventDefault();
+	win.kcPopHelp = function() {
+		win.kcHelpBox.css({
+			width: win.kcBody.width() * .85,
+			height: win.kcBody.height() * .85
+		})
+		.lightbox_me({
+			centered: true,
+			destroyOnClose: true,
+			showOverlay: true,
+			overlaySpeed: 10,
+			lightboxSpeed: 10,
+			overlayCSS: {
+				background: '#fff',
+				opacity: '.1'
 			}
 		});
-	}
-});
+	};
+
+
+	$(document).ready(function($) {
+		var $main	= $('.contextual-help-tabs-wrap').children(),
+				$side	= $('.contextual-help-sidebar'),
+				help	= '';
+
+		if ( $main.length ) {
+			$main.each(function() {
+				help += '<h3 class="title">'+ $.trim( $('#tab-link-'+this.id.replace('tab-panel-', '')).text() ) +'</h3>';
+				help += $(this).html();
+			});
+		}
+		// Side help
+		if ( $side.children().length ) {
+			help += '<hr />'+ $side.html();
+		}
+
+		if ( help !== '' ) {
+			win.kcHelpBox = $('<div id="kc-help-popup" class="hidden"><div class="_wrap"><div class="_inside">'+help+'</div></div></div>').appendTo(win.kcBody);
+
+			$(document).bind('keypress', function(e) {
+				if ( !$(e.target).is(':input') && e.which == 63 ) {
+					if ( win.kcHelpBox.is(':visible') )
+						win.kcHelpBox.trigger('close');
+					else
+						win.kcPopHelp();
+
+					e.preventDefault();
+				}
+			});
+		}
+	});
+})(jQuery);
+
