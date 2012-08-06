eb858b7e9ad6d35da5cac6913b1f9b15385d780e
Dzikri Aziz <kvcrvt@gmail.com>
2012-08-07 02:34:32 +0700

    Widget Logic: Accept arguments

--- kc-essentials-inc/widget_logic.php
+++ kc-essentials-inc/widget_logic.php
@@ -73,9 +73,11 @@ class kcEssentials_widget_logic {
 		<?php echo kcForm::field( array(
 			'type'    => 'select',
 			'attr'    => array(
-				'id'       => $f_id,
-				'name'     => "{$f_name}[]",
-				'multiple' => true
+				'id'         => $f_id,
+				'name'       => "{$f_name}[]",
+				'class'      => 'hasdep',
+				'multiple'   => true,
+				'data-child' => ".{$f_id}-args"
 			),
 			'options' => array(
 				'is_home'              => __('Homepage', 'kc-essentials'),
@@ -84,13 +86,12 @@ class kcEssentials_widget_logic {
 				'is_page'              => __('Page', 'kc-essentials'),
 				'is_page_template'     => __('Custom page template', 'kc-essentials'),
 				'is_single'            => __('Single post', 'kc-essentials'),
-				'is_sticky'            => __('Sticky post', 'kc-essentials'),
 				'is_attachment'        => __('Attachment', 'kc-essentials'),
 				'is_archive'           => __('Archive', 'kc-essentials'),
 				'is_post_type_archive' => __('Post type archive', 'kc-essentials'),
 				'is_category'          => __('Category', 'kc-essentials'),
 				'is_tag'               => __('Tag', 'kc-essentials'),
-				'is_tax'               => __('Taxonomy', 'kc-essentials'),
+				'is_tax'               => __('Taxonomy term', 'kc-essentials'),
 				'is_author'            => __('Author', 'kc-essentials'),
 				'is_404'               => __('404', 'kc-essentials'),
 				'is_search'            => __('Search page', 'kc-essentials'),
@@ -109,6 +110,21 @@ class kcEssentials_widget_logic {
 		) );
 		?>
 	</p>
+	<?php
+		$args_id   = $widget->get_field_id('kc-logic-args');
+		$args_name = $widget->get_field_name('kc-logic-args');
+		foreach ( array(
+			'is_page', 'is_single', 'is_singular', 'is_attachment'
+			,'is_category', 'is_tag', 'is_tax', 'is_author', 'is_page_template'
+			,'is_post_type_archive'
+		) as $cond ) {
+			$args_val = isset($setting['kc-logic-args'][$cond]) ? $setting['kc-logic-args'][$cond] : '';
+	?>
+	<p class="<?php echo $f_id ?>-args" data-dep="<?php echo $cond ?>">
+		<label for="<?php echo "{$args_id}-{$cond}" ?>"><?php printf( __('%s argument:', 'kc-essentials'), "<code>{$cond}</code>" ) ?></label>
+		<input id="<?php echo "{$args_id}-{$cond}" ?>" name="<?php echo "{$args_name}[{$cond}]" ?>" class="widefat" type="text" value="<?php esc_attr_e($args_val) ?>" />
+	</p>
+	<?php } ?>
 </div>
 	<?php
 		$return = null;
@@ -118,7 +134,7 @@ class kcEssentials_widget_logic {
 	public static function _save( $instance, $new, $old, $widget ) {
 		$setting = kcEssentials_widgets::get_setting( $widget->id );
 		$setting['kc-logic-enable'] = ( isset($new['kc-logic-enable']) && $new['kc-logic-enable'] ) ? true : false;
-		foreach ( array('kc-logic', 'kc-logic-mode') as $field )
+		foreach ( array('kc-logic', 'kc-logic-mode', 'kc-logic-args') as $field )
 			if ( isset($new[$field]) )
 				$setting[$field] = $new[$field];
 
@@ -152,7 +168,18 @@ class kcEssentials_widget_logic {
 
 				$show = ( $settings[$widget]['kc-logic-mode'] && $settings[$widget]['kc-logic-mode'] == 'show' ) ? true : false;
 				foreach ( $settings[$widget]['kc-logic'] as $func ) {
-					if ( call_user_func($func) === $show )
+					if ( isset( $settings[$widget]['kc-logic-args'][$func] ) && !empty($settings[$widget]['kc-logic-args'][$func]) ) {
+						$args =
+							strpos($settings[$widget]['kc-logic-args'][$func], ',') === true
+							? explode(',', $settings[$widget]['kc-logic-args'][$func])
+							: $settings[$widget]['kc-logic-args'][$func];
+					$res = call_user_func( $func, $args );
+					}
+					else {
+						$res = call_user_func( $func );
+					}
+
+					if ( $res === $show )
 						continue 2;
 				}
 
